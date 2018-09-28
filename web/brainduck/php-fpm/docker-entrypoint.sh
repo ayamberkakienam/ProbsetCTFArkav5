930d6936fb1131a72bc78520f08d23b82033544d
Muhamad Visat Sutarno <mvisat@gmail.com>
2018-09-28 14:54:13 +0700

    brainduck: directory permission issue, now www-data can't delete index.php

--- web/brainduck/php-fpm/docker-entrypoint.sh
+++ web/brainduck/php-fpm/docker-entrypoint.sh
@@ -3,8 +3,10 @@ set -e
 
 if [ "$1" = 'php-fpm' ]; then
     mkdir -p /var/www/public/uploads
+    chmod 1770 /var/www/public/uploads
+    chown -R root:www-data /var/www/public/uploads
     touch /var/www/public/uploads/index.php
-    chmod 777 /var/www/public/uploads
+    chmod 000 /var/www/public/uploads/index.php
 fi
 
 exec "$@"
