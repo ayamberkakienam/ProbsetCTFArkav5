7da4196df5b1e50e5855cd1d3fcd91e8273f76f2
rozium <fahrurrozi31@gmail.com>
2019-02-06 11:07:36 +0700

    add superez web chall: optimus-prime

---
+++ web/optimus-prime/index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Optimus Prime</title>
+	<style type="text/css">
+		body {
+			text-align: center;
+			margin-top: 7%;
+			font-family: "Sans-serif";
+		}
+		img {
+			width: 40%;
+		}
+	</style>
+</head>
+<body>
+	<img src="robots.jpg" alt="optimus" />
+	<blockquote>“In any war, there are calms between storms. There will be days when we lose faith. <br /> Days when our allies turn against us...but the day will never come that we forsake this planet and its people.” <br /> ― Optimus Prime</blockquote>
+</body>
+</html>
---
+++ web/optimus-prime/mysecret.php
@@ -0,0 +1,24 @@
+<?php
+	header('flag: Arkav5{freedom_is_the_right_of_all_sentient_beings__}')
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+	<title>My Secret</title>
+	<style type="text/css">
+		body {
+			text-align: center;
+			margin-top: 7%;
+			font-family: "Sans-serif";
+		}
+		img {
+			width: 600px;
+		}
+	</style>
+</head>
+<body>
+	<img src="head.jpg" alt="head" />
+	<blockquote>“There's a thine line between being a hero and being a memory.” <br /> ― Optimus Prime</blockquote>
+</body>
+</html>
